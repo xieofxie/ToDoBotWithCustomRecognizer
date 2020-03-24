cfc61745229374f8aff7b62f4b6e29925c45760d
Hualiang Xie <hualxie@microsoft.com>
2020-03-24 10:48:36 +0800

    update. change to port 9000

--- Tools/server.py
+++ Tools/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, current_app
+from flask_cors import CORS
+from flask import Flask, request, current_app, make_response
 import spacy
 import os
 import uuid
@@ -8,22 +9,33 @@ from datetime import datetime
 import threading
 import time
 import uuid
+import json
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 id_models = dict()
 
+def add_response_header(content):
+    rst = make_response(content)
+    rst.headers['Access-Control-Allow-Origin'] = '*'
+    rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+    allow_headers = "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    rst.headers['Access-Control-Allow-Headers'] = allow_headers
+    return rst
+
 def response_403(id):
-    return 'App {0} is not updated correctly!'.format(id), 403
+    return add_response_header('App {0} is not updated correctly!'.format(id)), 403
 
 def response_404(id):
-    return 'App {0} does not exist!'.format(id), 404
+    return add_response_header('App {0} does not exist!'.format(id)), 404
 
 @app.route('/create_app')
 def create_app():
     id = str(uuid.uuid4())
     app_dir = common.get_app_dir(id)
     pathlib.Path(app_dir).mkdir(parents=True, exist_ok=True)
-    return id
+    return add_response_header(id), 201
 
 def update_app(id, data, files, is_create, is_force):
     app_dir = common.get_app_dir(id)
@@ -34,13 +46,13 @@ def update_app(id, data, files, is_create, is_force):
             return response_404(id)
     json_data = common.load_json(data, files, app_dir)
     if not is_force and common.compare_json(json_data, app_dir):
-        return id
+        return add_response_header(id), 201
     common.remove_json(app_dir)
     nlp_c = train_textcat.main(json_data, output_dir=common.get_category_dir(app_dir))
     nlp_e = train_new_entity_type.main(json_data, output_dir=common.get_entity_dir(app_dir))
     id_models[id] = (nlp_c, nlp_e)
     common.save_json(json_data, app_dir)
-    return id
+    return add_response_header(id), 201
 
 def update_app_async(id, data, files, is_create, is_force):
     return update_app(id, data, files, is_create, is_force)
@@ -75,10 +87,11 @@ def query_app(id, query):
     doc_category = nlp_c(query)
     doc_entity = nlp_e(query)
     print(query, doc_category.cats, doc_entity.ents)
-    return {
+    result = {
         "cats": doc_category.cats,
         "ents": [(ent.label_, ent.text) for ent in doc_entity.ents],
     }
+    return add_response_header(result), 201
 
 @app.route('/query_app/<id>/<query>', methods=['GET'])
 def query_app_get(id, query):
@@ -89,4 +102,4 @@ def query_app_post(id):
     return query_app(id, str(request.get_json()['query']))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=9000, debug=True)
